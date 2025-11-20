bd2d05517468b2d980616ff8f30eae094fc1c21b
vamsikrishna976 <paluruvamsikrishna16@gmail.com>
2025-11-20 14:21:05 +0530

    removed vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React, {useEffect, useState, useMemo} from 'react'
-import { Analytics } from "@vercel/analytics/next"
 import Header from './components/Header'
 import JobCard from './components/JobCard'
 import Pagination from './components/Pagination'
